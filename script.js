31225a733ad69a663b966a9b43b3e0c5743837b3
Joey <gibbonsjoey95@gmail.com>
2024-02-04 22:02:43 -0600

    got js started

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('Scripted')
+const addBtn = document.querySelector('#addBtn')
+
+const myLibrary = []
+
+function Book(title, author, page, read) {
+    this.title = title
+    this.author = author
+    this.page = page
+    this.read = read
+}
+
+Book.prototype.introduce = function() {
+    console.log(`${this.title} by ${this.author}, ${this.page} pages. ${this.read}`)
+}
+
+book1 = new Book('Eragon', 'Yada', 300, 'I have not read this book.')
+
+addBtn.addEventListener('click', () => {
+    book1.introduce()
+})
+
