72235d6b26d548675f24d37e67885f776fd9c58f
Joey <gibbonsjoey95@gmail.com>
2024-02-05 21:45:23 -0600

    set up array to contain book objects to create table from

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 const addBtn = document.querySelector('#addBtn')
+const bookContainer = document.querySelector('#bookContainer')
 
-const myLibrary = []
+
+const myLibrary = [
+    {
+        title: "Fahrenheit 451",
+        author: "Ray Bradbury",
+        page: 249,
+        read: false
+    },
+    {
+        title: "Running Out Of Time",
+        author: "Margaret Peterson Haddix",
+        page: 184,
+        read: true
+    }
+]
 
 function Book(title, author, page, read) {
     this.title = title
@@ -8,6 +23,23 @@ function Book(title, author, page, read) {
     this.page = page
     this.read = read
 }
+
+myLibrary.forEach((book) => {
+    let row = bookContainer.insertRow(0)
+
+    let cellTitle = row.insertCell(0)
+    let cellAuthor = row.insertCell(1)
+    let cellPage = row.insertCell(2)
+    let cellRead = row.insertCell(3)
+
+    cellTitle.textContent = book.title
+    cellAuthor.textContent = book.author
+    cellPage.textContent = book.page
+
+    book.read === false ? cellRead.textContent = "No" : cellRead.textContent = "Yes"
+
+    console.log(book.read ? 'Yes' : "no")
+})
 
 Book.prototype.introduce = function() {
     console.log(`${this.title} by ${this.author}, ${this.page} pages. ${this.read}`)
