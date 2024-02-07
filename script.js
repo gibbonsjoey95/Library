b2b981098c48f3cf30d608dbed6711b7fde4c973
Joey <gibbonsjoey95@gmail.com>
2024-02-06 22:06:38 -0600

    working on adding books to table

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ const addBtn = document.querySelector('#addBtn')
 const bookContainer = document.querySelector('#bookContainer')
 const modal = document.querySelector('.modal')
 const closeModal = document.querySelector('.close')
+const bookForm = document.querySelector('#bookForm')
+const title = document.querySelector('#title')
+const author = document.querySelector('#author')
+const pages = document.querySelector('#pages')
+const read = document.querySelector('#read')
+
+// const tableBody = document.querySelector('#table_of_items tr')
 
 const myLibrary = [
     {
@@ -25,26 +32,32 @@ function Book(title, author, page, read) {
     this.read = read
 }
 
-myLibrary.forEach((book) => {
-    let row = bookContainer.insertRow(0)
 
-    let cellTitle = row.insertCell(0)
-    let cellAuthor = row.insertCell(1)
-    let cellPage = row.insertCell(2)
-    let cellRead = row.insertCell(3)
+const addBookToLibrary = () => {
+    // bookContainer.remove()
 
-    cellTitle.textContent = book.title
-    cellAuthor.textContent = book.author
-    cellPage.textContent = book.page
+    myLibrary.forEach((book) => {
+        let row = bookContainer.insertRow(0)
+        
+        let cellTitle = row.insertCell(0)
+        let cellAuthor = row.insertCell(1)
+        let cellPage = row.insertCell(2)
+        let cellRead = row.insertCell(3)
+        
+        cellTitle.textContent = book.title
+        cellAuthor.textContent = book.author
+        cellPage.textContent = book.page
+        
+        book.read === false ? cellRead.textContent = "No" : cellRead.textContent = "Yes"
+    })
 
-    book.read === false ? cellRead.textContent = "No" : cellRead.textContent = "Yes"
-})
+    console.log('yes??')
+}
+
 
 Book.prototype.introduce = function() {
     console.log(`${this.title} by ${this.author}, ${this.page} pages. ${this.read}`)
 }
-
-book1 = new Book('Eragon', 'Yada', 300, 'I have not read this book.')
 
 addBtn.addEventListener('click', () => {
     modal.style.display = 'block'
@@ -54,3 +67,14 @@ closeModal.addEventListener('click', () => {
     modal.style.display = 'none'
 })
 
+bookForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    let newBook = new Book(title.value, author.value, pages.value, read.checked)
+    myLibrary.push(newBook)
+    addBookToLibrary()
+
+    modal.style.display = 'none'
+})
+
+addBookToLibrary()
